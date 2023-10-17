ac10857a2497d403db7347393a1d74401b228f9f
AllenAJ <allenxavier45@gmail.com>
2023-10-17 13:31:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import your CSS file
+import uniswapLogo from './uniswap-logo.png'; // Import the Uniswap logo image
+import { Swap } from '@swing.xyz/ui';
+import '@swing.xyz/ui/theme.css';
+import Navbar from './NavBar'; // Assuming Navbar component file is in the same directory
 
+
+/**
+ * Renders the main application component.
+ * @returns {JSX.Element} The main application component.
+ */
+/**
+ * Renders the main application component.
+ * @returns {JSX.Element} The main application component.
+ */
 function App() {
   return (
+    <div className="">
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar />
       </header>
+      <main className="main-boby">
+      
+        <Swap projectId="allen-joseph" />
+
+        
+      </main>
+      </div>
     </div>
   );
 }
